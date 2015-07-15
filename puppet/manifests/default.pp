b6b0496f8ac478ff53ab6d444deef932d28e96f3
George H. Peden <georgep@slalom.com>
2015-07-14 17:10:02 -0700

    create target dirs and source files for node

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -19,9 +19,26 @@ file { "/var/www/html/sample-webapp":
   notify => Service["apache2"],
 }
 
+# Node.js app stuff
 class { 'nodejs': }
 
 class { '::mysql::server':
   root_password  => 'foo',
 }
 
+file {["/opt/nodejs", "/opt/nodejs/sample-node"]:,
+  ensure => "directory",
+  mode => 775,
+}
+
+file { "/opt/nodejs/sample-node/app.js":
+  ensure => "link",
+  target => "/vagrant/sample-node/app.js",
+  require => File["/opt/nodejs/sample-node"],
+}
+
+file { "/opt/nodejs/sample-node/package.json":
+  ensure => "link",
+  target => "/vagrant/sample-node/package.json",
+  require => File["/opt/nodejs/sample-node"],
+}
