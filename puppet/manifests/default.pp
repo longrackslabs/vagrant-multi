e64719f809765e8aa6fde21fc6a8272c607b6c10
George H. Peden <georgep@slalom.com>
2015-07-24 15:04:47 -0700

    Ruby installed

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -20,3 +20,75 @@ file { "/var/www/html/sample-webapp":
   require => Package["apache2"],
   notify => Service["apache2"],
 }
+
+# Ruby stuff
+class ruby {
+  package { "build-essential":
+    ensure => present,
+    require => Exec["apt-get update"],
+  }
+
+  package { "ruby":
+    ensure => present,
+    require => Exec["apt-get update"],
+  }
+}
+
+class thin {
+
+    # validate_platform() function comes from
+    # puppet module gajdaw/diverse_functions
+    #
+    #     https://forge.puppetlabs.com/gajdaw/diverse_functions
+    #
+    if !validate_platform($module_name) {
+        fail("Platform not supported in module '${module_name}'.")
+    }
+
+    Exec { path => [
+        '/usr/local/sbin',
+        '/usr/local/bin',
+        '/usr/sbin',
+        '/usr/bin',
+        '/sbin',
+        '/bin'
+    ]}
+
+    exec { 'sinatra:install':
+        command => 'gem install sinatra',
+        timeout => 6000,
+    }
+
+    exec { 'sinatra:thin':
+        command => 'gem install thin',
+    }
+
+}
+
+# Then install the app bits
+class install-sample-ruby-app {
+
+  file {"/opt/ruby":
+    ensure => "directory",
+    mode => 775,
+  }
+
+  file {"/opt/ruby/sample-ruby":
+    ensure => "directory",
+    mode => 775,
+  }
+
+  file { "/opt/ruby/sample-ruby/hello-ruby.rb":
+    ensure => "link",
+    target => "/vagrant/sample-ruby/hello-ruby.rb",
+    require => [
+      File["/opt/ruby"],
+      File["/opt/ruby/sample-ruby"]
+    ]
+  }
+
+}
+
+class { 'install-sample-ruby-app': }
+
+include apt
