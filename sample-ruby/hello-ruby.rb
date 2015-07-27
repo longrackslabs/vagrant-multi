363c9ab9833e1c8496191df3718e14eb25508d38
George H. Peden <georgep@slalom.com>
2015-07-27 13:15:07 -0700

    add /quote REST API endpoint

--- sample-ruby/hello-ruby.rb
+++ sample-ruby/hello-ruby.rb
@@ -1,9 +1,26 @@
 # hello-ruby.rb
 require 'sinatra'
+require 'json'
 
 set bind: '0.0.0.0'
 
 get '/' do
   time1 = Time.new
   'Hello World: Ruby From Vagrant! ' + time1.inspect
+end
+
+class DudeQuoteServer
+  attr_reader :quotes
+  def initialize
+    @quotes = %w(Just one thing, Dude. Do ya have to use so many cuss words?)
+    @quotes.concat %w(Hey, this is a private residence man.)
+  end
+end
+
+# Sinatra Part
+get '/quote' do
+  return_message = {}
+  return_message[:quote] = 'Hello. Mein dizbatcher says zere iss somezing wrong mit deine kable.'
+
+  return_message.to_json
 end
