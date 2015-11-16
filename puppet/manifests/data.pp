a93257ab5484f25cc4034fb30f30b7d0f498e1dc
George H. Peden <georgep@slalom.com>
2015-11-15 17:33:09 -0800

    initial working ansible / vagrant setup for multi-vm

--- puppet/manifests/data.pp
+++ puppet/manifests/data.pp
@@ -1,32 +1,32 @@
 
 
 # Then install the app bits
-class install-sample-ruby-app {
+class install-data-app {
 
   file {"/opt/ruby":
     ensure => "directory",
     mode => 775,
   }
 
-  file {"/opt/ruby/sample-ruby":
+  file {"/opt/ruby/data":
     ensure => "directory",
     mode => 775,
   }
 
-  file { "/opt/ruby/sample-ruby/hello-ruby.rb":
+  file { "/opt/ruby/data/hello-ruby.rb":
     ensure => "link",
-    target => "/vagrant/sample-ruby/hello-ruby.rb",
+    target => "/vagrant/data/hello-ruby.rb",
     require => [
       File["/opt/ruby"],
-      File["/opt/ruby/sample-ruby"]
+      File["/opt/ruby/data"]
     ]
   }
 
-  file { "/opt/ruby/sample-ruby/start.sh":
+  file { "/opt/ruby/data/start.sh":
     ensure => "link",
-    target => "/vagrant/sample-ruby/start.sh",
+    target => "/vagrant/data/start.sh",
     mode => 775,
-    require => File["/opt/ruby/sample-ruby/hello-ruby.rb"],
+    require => File["/opt/ruby/data/hello-ruby.rb"],
   }
 
 }
@@ -49,12 +49,18 @@ class sinatra-jsonp {
 
 class {'sinatra-jsonp':}
 
+#class { 'ansible':
+#  ensure => master
+#}
+#class { 'ansible':}
+
+
 # Ruby stuff
 class { 'ruby':
   gems_version  => 'latest'
 }
 
-class { 'install-sample-ruby-app':
+class { 'install-data-app':
   require => [
     Class['ruby'],
     Class['sinatra'],
